2ede7b311808e3af495033ad537a03311fc7d262
Garry Ing <garry.ing@gmail.com>
2016-12-21 08:22:31 -0500

    fixed url base

--- api/slack.rb
+++ api/slack.rb
@@ -11,6 +11,6 @@ def slack_notifier_base(story_timestamp)
   {
     ts: story_timestamp,
     footer: 'Are.na',
-    footer_icon: @arena_url_base + 'favicon.ico'
+    footer_icon: @arena_url + 'favicon.ico'
   }
 end
